f4f50f198b2c4133a3fe0ece4a0d34d4162c47dc
TayfurSafakGencay <safak.gencay@gmail.com>
2025-04-21 08:06:21 +0300

    Default data

---
+++ Assets/Scripts/Weapon/DefaultData/DefaultAttributeData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Weapon.Attributes.Base;
+
+namespace Weapon.DefaultData
+{
+	[CreateAssetMenu(fileName = "DefaultAttributeData", menuName = "Scriptable Objects/Gun/Attribute/Create a Attribute Data", order = 0)]
+	public class DefaultAttributeData : ScriptableObject
+	{
+		[SerializeField]
+		private List<GunAttribute>_attributes;
+
+		public Dictionary<AttributeType, GunAttribute> AttributeType;
+		
+		private bool _isInitialized;
+
+		private void OnEnable()
+		{
+			_isInitialized = false;
+
+			Initialize();
+		}
+		
+		public void Initialize()
+		{
+			if (_isInitialized) return;
+			_isInitialized = true;
+
+			AttributeType = new Dictionary<AttributeType, GunAttribute>();
+
+			foreach (GunAttribute attribute in _attributes)
+			{
+				AttributeType.TryAdd(attribute.AttributeDto.AttributeType, attribute);
+			}
+		}
+
+		public AttributeDTO GetAttributeData(AttributeType attributeType)
+		{
+			if (AttributeType.TryGetValue(attributeType, out GunAttribute data)) return data.AttributeDto;
+
+			Debug.LogWarning($"Gun with name {attributeType} not found!");
+			return new AttributeDTO();
+		}
+	}
+}
---
+++ Assets/Scripts/Weapon/DefaultData/DefaultGunData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Weapon.Base;
+
+namespace Weapon.DefaultData
+{
+	[CreateAssetMenu(fileName = "DefaultGunData", menuName = "Scriptable Objects/Gun/UnsavedData", order = 0)]
+	public class DefaultGunData : ScriptableObject
+	{
+		[SerializeField]
+		private List<DefaultGunDTO> _guns;
+
+		private Dictionary<string, DefaultGunDTO> _gunsDictionary;
+
+		private bool _isInitialized;
+
+		private async void OnEnable()
+		{
+			_isInitialized = false;
+
+			await Initialize();
+		}
+
+		public async Task Initialize()
+		{
+			if (_isInitialized) return;
+			_isInitialized = true;
+
+			_gunsDictionary = new Dictionary<string, DefaultGunDTO>();
+
+			foreach (DefaultGunDTO gun in _guns)
+			{
+				_gunsDictionary.TryAdd(gun.GunConfig.GunDTO.Name, gun);
+			}
+
+			await UniTask.Delay(50);
+		}
+
+		public DefaultGunDTO GetGunData(string gunName)
+		{
+			if (_gunsDictionary.TryGetValue(gunName, out DefaultGunDTO data)) return data;
+
+			Debug.LogWarning($"Gun with name {gunName} not found!");
+			return new DefaultGunDTO();
+		}
+	}
+	
+	[Serializable]
+	public struct DefaultGunDTO
+	{
+		public GunConfig GunConfig;
+		
+		public Sprite Icon;
+		
+		public Gun Gun;
+		
+		public Bullet Bullet;
+
+	}
+}
